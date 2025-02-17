4ddc103e03c4a52ec0b044306fc164f5ed562509
AbhigyanPie <abhigyantripathi729@gmail.com>
2025-02-17 12:19:36 +0530

    updated code

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,3 @@
-// import TextEditor from "./TextEditor";
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-//   Navigate
-// } from 'react-router-dom';
-// import { v4 as uuidV4 } from 'uuid';  
-
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         {/* Replace Redirect with Navigate */}
-//         <Route path="/" element={<Navigate to={`/documents/${uuidV4()}`} />} />
-        
-//         {/* Use element prop for TextEditor */}
-//         <Route path="/documents/:id" element={<TextEditor />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
